50d416b2572bb396d69772145d1adf4326bb4718
rejo1234 <kamilfranczak@onet.pl>
2024-09-26 01:24:04 +0200

    zadanie2

--- src/poker/Poker.java
+++ src/poker/Poker.java
@@ -6,14 +6,14 @@ import java.util.*;
 
 public class Poker {
 
-    private EquityEvaluator myequityEvaluator;
-
     public void init() {
         Deck myDeck = new Deck();
         List<Card> board = myDeck.getBoard("2c", "2d", "9c", "4c", "9d");
-        List<Card> hand = myDeck.getHand("Ah", "Kd");
-        List<Card> hand2 = myDeck.getHand("9s", "Th");
+        List<Card> hand = myDeck.getHand("2h", "2s");
+        List<Card> hand2 = myDeck.getHand("3d", "3s");
+        String filePath = "equity_data.ser";
         EquityEvaluator myequityEvaluator = new EquityEvaluator(myDeck, hand, hand2, board);
+        myequityEvaluator.loadHashMapFromFile(filePath);
         Card cardFLop1 = new Card(10, "s");
         Card cardFLop2 = new Card(9, "c");
         Card cardFLop3 = new Card(14, "c");
@@ -27,7 +27,9 @@ public class Poker {
         flopAndTurn.add(cardTurn4);
         //myequityEvaluator.calculateEquityTurn(flopAndTurn);
          //myequityEvaluator.calculateEquityTurn(flopAndTurn);
-         myequityEvaluator.calculateEquity();
+        //myequityEvaluator.calculateEquity();
         // myequityEvaluator.countTime();
+        //myequityEvaluator.instantlyResult();
+        myequityEvaluator.getEquity(hand,hand2);
     }
 }
